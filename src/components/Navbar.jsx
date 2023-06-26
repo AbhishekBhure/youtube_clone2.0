74434f615ae0c72f28e0ca451ee7f721fcebf69f
AbhishekBhure <abhiab7411@gmail.com>
2023-06-26 02:23:00 -0700

    set up --components and utils

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { Stack } from "@mui/material";
+import { Link } from "react-router-dom";
+
+const Navbar = () => {
+  return <div>Navbar</div>;
+};
+
+export default Navbar;
