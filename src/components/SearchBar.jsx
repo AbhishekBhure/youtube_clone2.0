1f9248e052c96b2722f421c2eb1f058de2bac840
AbhishekBhure <abhiab7411@gmail.com>
2023-06-28 04:53:13 -0700

    done searchBar to search videos

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -6,10 +6,21 @@ import { Search } from "@mui/icons-material";
 
 const SearchBar = () => {
   const navigate = useNavigate();
+
+  const [searchTerm, setSearchTerm] = useState([]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (searchTerm) {
+      navigate(`/search/${searchTerm}`);
+
+      setSearchTerm("");
+    }
+  };
   return (
     <Paper
       component="form"
-      onSubmit={() => {}}
+      onSubmit={handleSubmit}
       sx={{
         borderRadius: 20,
         border: "1px solid #e3e3e3",
@@ -21,8 +32,8 @@ const SearchBar = () => {
       <input
         className="search-bar"
         placeholder="Search..."
-        value=""
-        onChange={() => {}}
+        value={searchTerm}
+        onChange={(e) => setSearchTerm(e.target.value)}
       />
       <IconButton
         type="submit"
