9629de5af15fa27ea04756c27a642359915807a3
AbhishekBhure <abhiab7411@gmail.com>
2023-07-04 23:27:22 -0700

    error in VideoDetail Component

--- src/components/VideoDetail.jsx
+++ src/components/VideoDetail.jsx
@@ -16,8 +16,14 @@ const VideoDetail = () => {
     fetchFromAPI(`videos?part=snippet,statistics&id=${id}`).then((data) =>
       setVideoDetail(data.items[0])
     );
-  }, []);
+  }, [id]);
 
+  if (!videoDetail?.snippet) return "Loading...";
+
+  const {
+    snippet: { title, channelId, channelTitle },
+    statistics: { viewCount, likeCount },
+  } = videoDetail;
   return (
     <Box minHeight="95vh">
       <Stack direction={{ xs: "column", md: "row" }}>
@@ -28,6 +34,9 @@ const VideoDetail = () => {
               className="react-player"
               controls
             />
+            <Typography color="#fff" variant="h5" fontWeight="bold" p={2}>
+              {title}
+            </Typography>
           </Box>
         </Box>
       </Stack>
